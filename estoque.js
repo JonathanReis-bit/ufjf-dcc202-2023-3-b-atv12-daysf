3d5e4821128a9790d7d59216d4b575cf493c2a04
Yan Dias Ferreira <13732727750@estudante.ufjf.br>
2023-12-05 23:07:15 -0300

    reformulei funções e corigi erros de sintaxe

--- estoque.js
+++ estoque.js
@@ -1,12 +1,12 @@
 let estoque = {
     'joao': [
-        {'tipo': 'maca', 'qnt': 1},
-        {'tipo': 'pera', 'qnt': 2}
+        {'tipo': 'maca', 'qtd': 1},
+        {'tipo': 'pera', 'qtd': 2}
 
     ],
     'maria': [
-        {'tipo': 'maca', 'qnt': 2},
-        {'tipo': 'banana', 'qnt': 4}
+        {'tipo': 'maca', 'qtd': 2},
+        {'tipo': 'banana', 'qtd': 4}
 
     ]
 };
@@ -17,8 +17,48 @@ function getEstoque(){
 
 
 function transacao(origem, destino, tipo, quantidade){
+    if(origem === destino) {return; }
     if(destino === "pomar"){
-            const pessoa = estoque[origem];
+        dePessoaParaPomar(origem, tipo, quantidade);
+        return;
+    }
+
+
+    if(origem === "pomar"){
+        dePomarParaPessoa(destino, tipo, quantidade);
+        return;
+}
+    const pessoaOrigem = estoque[origem];
+    const pessoaDestino = estoque[destino];
+    let monteOrigem;
+    for (let i=0; i < pessoaOrigem.lenght; i++){
+        const monte = pessoaOrigem[i];
+        if(monte.tipo === tipo){
+            monteOrigem = monte;
+            break;
+        }
+    }
+
+if (!monteOrigem) { return; }
+let monteDestino;
+    for(let i=0; i < pessoaDestino.lenght; i++){
+        const monte = pessoaDestino[i];
+        if(monte.tipo === tipo){
+            monteDestino = monte;
+            break;
+        }
+    }
+    if (!monteDestino){
+        monteDestino = {'tipo' : tipo, qtd: 0};
+        pessoaDestino.push(monteDestino);
+    }
+    const qtdReal = Math.min(quantidade, monteOrigem.qtd);
+    monteDestino.qtd += qtdReal;
+    monteOrigem.qtd -= qtdReal;
+}
+
+function dePessoaParaPomar(origem, tipo, quantidade){
+    const pessoa = estoque[origem];
             for(let i=0; i<pessoa.length; i++){
                 const monte = pessoa[i];
                 if(monte.tipo === tipo){
@@ -26,16 +66,9 @@ function transacao(origem, destino, tipo, quantidade){
                     return;
                 }
             }
-    }
-
-
-    if(origem === "pomar"){
-        dePomarParaPessoa(destino, quantidade);
-
 }
 
-
-function dePomarParaPessoa(){
+function dePomarParaPessoa(destino, tipo, quantidade){
     const pessoa = estoque[destino];
             for(let i=0; i<pessoa.length; i++){
                 const monte = pessoa[i];
@@ -47,6 +80,6 @@ function dePomarParaPessoa(){
             const novoMonte = {'tipo': tipo, 'qtd': Math.max(quantidade, 0)};
             pessoa.push(novoMonte);
     }
-}
+
 
 export {getEstoque, transacao};
